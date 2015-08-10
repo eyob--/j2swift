c81a4c8141d7d1b5b90f3e7cfaaed9e6f84d6978
eyob-- <tse.eyob@gmail.com>
2015-08-10 15:51:02 -0400

    Add simple file write code

--- src/J2Swift.java
+++ src/J2Swift.java
@@ -3,26 +3,30 @@ import org.antlr.v4.runtime.tree.*;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 /**
  * Main class for the Java to Swift converter
  * @author Eyob Tsegaye
  */
-public class J2Swift
-{
-	public static void main( String [] args ) throws IOException
-    {
-        String inputFile = null;
-        if ( args.length>0 ) inputFile = args[0];
-        InputStream is = System.in;
-        if ( inputFile!=null ) { is = new FileInputStream(inputFile); }
-        ANTLRInputStream input = new ANTLRInputStream(is);
-        Java8Lexer lexer = new Java8Lexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        Java8Parser parser = new Java8Parser(tokens);
-        ParserRuleContext tree = parser.compilationUnit();
-        ParseTreeWalker walker = new ParseTreeWalker();
-        J2SwiftListener listener = new J2SwiftListener();
-        walker.walk(listener, tree);
+public class J2Swift {
+	public static void main(String[] args) throws IOException {
+		String inputFile = null;
+		if (args.length > 0) inputFile = args[0];
+		InputStream is = System.in;
+		if (inputFile != null)
+			is = new FileInputStream(inputFile);
+		ANTLRInputStream input = new ANTLRInputStream(is);
+		Java8Lexer lexer = new Java8Lexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		Java8Parser parser = new Java8Parser(tokens);
+		ParserRuleContext tree = parser.compilationUnit();
+		ParseTreeWalker walker = new ParseTreeWalker();
+		J2SwiftListener listener = new J2SwiftListener();
+		walker.walk(listener, tree);
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(inputFile+".swift")));
+		pw.println(listener.swiftCode());
 	}
 }
--- src/J2SwiftListener.java
+++ src/J2SwiftListener.java
@@ -5,4 +5,13 @@
  */
 public class J2SwiftListener extends Java8BaseListener {
 
+    /**
+     * Returns the swift code to be outputted to a file, or null if
+     * the tree hasn't been walked with this listener.
+     * @return swift code as a giant String
+     */
+    public String swiftCode() {
+        return null;
+    }
+
 }
