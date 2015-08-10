4e22fb57030244305b2e4d1d91e92ef0d2776f8f
eyob-- <tse.eyob@gmail.com>
2015-08-10 14:44:40 -0400

    Add Boilerplate code

---
+++ src/J2Swift.java
@@ -0,0 +1,28 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
+/**
+ * Main class for the Java to Swift converter
+ * @author Eyob Tsegaye
+ */
+public class J2Swift
+{
+	public static void main( String [] args ) throws IOException
+    {
+        String inputFile = null;
+        if ( args.length>0 ) inputFile = args[0];
+        InputStream is = System.in;
+        if ( inputFile!=null ) { is = new FileInputStream(inputFile); }
+        ANTLRInputStream input = new ANTLRInputStream(is);
+        Java8Lexer lexer = new Java8Lexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        Java8Parser parser = new Java8Parser(tokens);
+        ParserRuleContext tree = parser.compilationUnit();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        J2SwiftListener listener = new J2SwiftListener();
+        walker.walk(listener, tree);
+	}
+}
---
+++ src/J2SwiftListener.java
@@ -0,0 +1,8 @@
+/**
+ * Actual "behind-the-scenes" java to swift converter that processes
+ * the parse tree for the java file and gives back swift code
+ * @author Eyob Tsegaye
+ */
+public class J2SwiftListener extends Java8BaseListener {
+
+}
