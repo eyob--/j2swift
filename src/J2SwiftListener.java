569543b0f3a8d9e0cc54a4d9ee8e549a0612f0ae
eyob-- <tse.eyob@gmail.com>
2015-08-11 00:39:21 -0400

    Add starter code

--- src/J2SwiftListener.java
+++ src/J2SwiftListener.java
@@ -5,13 +5,15 @@
  */
 public class J2SwiftListener extends Java8BaseListener {
 
+    private String code = "";
+
     /**
-     * Returns the swift code to be outputted to a file, or null if
+     * Returns the swift code to be outputted to a file, or the empty string if
      * the tree hasn't been walked with this listener.
      * @return swift code as a giant String
      */
     public String swiftCode() {
-        return null;
+        return code.isEmpty() ? code : "import Foundation\n\n" + code;
     }
 
 }
